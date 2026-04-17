03c46ab14834dadee2d0c72704e37f1a4ff31a39
satya <psatyashry@gmail.com>
2026-04-17 22:57:41 +0530

    notebook

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name="CreditCard",
+    name="StudentPerformance",
     version= "0.0.1",
     author= "Satya",
     author_email= "psatyashry@gmail.com",
