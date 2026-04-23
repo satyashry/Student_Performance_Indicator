3119356bb5f9fdf8f9e8e73f9758df2a19096a95
satya <psatyashry@gmail.com>
2026-04-23 11:25:06 +0530

    logger

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,20 @@ import logging
 import os 
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+folder_name = datetime.now().strftime('%m_%d_%Y')
+logs_path = os.path.join(os.getcwd(), "logs", folder_name)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO,
-
+    level=logging.INFO,
 )
 
 if __name__ == "__main__":
-    logging.info("Loggin has started")
+    logging.info("Logging has started in the daily folder")
