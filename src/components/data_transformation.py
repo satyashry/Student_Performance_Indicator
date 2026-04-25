78c38850838b888bed69162e79a6bfee7ab5120e
satya <psatyashry@gmail.com>
2026-04-25 11:11:13 +0530

    Tranformation Function

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,4 @@
-import sys
+import sys,os
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
@@ -6,3 +6,67 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_ob_file_path = os.path.join('artifacts',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_transformer_object(self):
+        '''
+        the function responsible for data transformation
+        '''
+        try :
+            numerical_columns = ["writing_score","reading_score"]
+            categorical_columns =  [
+                'gender', 
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median"))
+                ("scaler",StandardScaler())
+                ]
+            )
+            cat_pipeline = Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent"))
+                ("one_hot_encoder",OneHotEncoder())
+                ("scaler",StandardScaler())
+                ]
+            )
+
+
+            logging.info(f"Numerical Columns: {numerical_columns}")
+            logging.info(f"Categorical Columns: {categorical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns)
+                ("cat_pipeline",cat_pipeline,categorical_columns)   
+                ]
+            )
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try :
+            train_df =pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Completed the Train and Test data")
+        except:
+            pass
+    
