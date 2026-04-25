6079140bb5e99763505f949811dd5124b8a3c98e
satya <psatyashry@gmail.com>
2026-04-25 12:01:04 +0530

    data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig :
@@ -20,7 +21,7 @@ class DataIngestion :
         logging.info("Entered the DataIngestion method or component")
 
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')  
             logging.info('Reading the Dataset as Dataframe')
 
                 
@@ -43,4 +44,7 @@ class DataIngestion :
                 
 if __name__== "__main__" :
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 
 @dataclass
@@ -34,15 +35,14 @@ class DataTransformation:
 
             num_pipeline = Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="median"))
+                ("imputer",SimpleImputer(strategy="median")),
                 ("scaler",StandardScaler())
                 ]
             )
             cat_pipeline = Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent"))
+                ("imputer",SimpleImputer(strategy="most_frequent")),
                 ("one_hot_encoder",OneHotEncoder())
-                ("scaler",StandardScaler())
                 ]
             )
 
@@ -52,7 +52,7 @@ class DataTransformation:
 
             preprocessor = ColumnTransformer(
                 [
-                ("num_pipeline",num_pipeline,numerical_columns)
+                ("num_pipeline",num_pipeline,numerical_columns),
                 ("cat_pipeline",cat_pipeline,categorical_columns)   
                 ]
             )
@@ -67,6 +67,46 @@ class DataTransformation:
             test_df = pd.read_csv(test_path)
 
             logging.info("Completed the Train and Test data")
-        except:
-            pass
+
+            logging.info("Obtaining Preprocessor Object")
+
+            preprocessing_obj = self.get_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score","reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df =  test_df.drop(columns=[target_column_name])
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("Applying preprocessing oject on training and testing Dataframe...")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+
+            test_arr = np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
+            ]
+
+            logging.info("saved Preprocessing object.")
+
+            
+            save_object (
+                file_path = self.data_transformation_config.preprocessor_ob_file_path,
+                obj =  preprocessing_obj # used for saving the object
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_ob_file_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
     
